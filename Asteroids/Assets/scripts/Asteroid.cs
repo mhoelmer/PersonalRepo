db566515e6e0937a51402f2e36e9d65f155eef5c
mhoelmer <mhoelmer@uccs.edu>
2017-04-01 22:25:51 -0600

    PI2 v2
    Added asteroid sprite/object

---
+++ Asteroids/Assets/scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An Asteroid
+/// </summary>
+public class Asteroid : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //apply impulse force
+        const float ImpulseForceRange = 2f;
+        const float MinImpulseForce = 3f;
+        GetComponent<Rigidbody2D>().AddForce(
+            Random.insideUnitCircle * ImpulseForceRange +
+            new Vector2(MinImpulseForce, MinImpulseForce),
+            ForceMode2D.Impulse);
+    }
+
+}
